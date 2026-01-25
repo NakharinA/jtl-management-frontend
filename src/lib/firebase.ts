331ffbacb3203dfab8ec750cbfce81320b7291c3
nakharin-work <nakharin.aroon.work@gmail.com>
2026-01-26 01:22:41 +0700

    feat: Set up Firebase authentication with environment variable configuration using dotenv in Vite.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDt8GjFPaHdS3Zzmw1nYl4ycLHJv_ZSYEg",
-  authDomain: "jtl-management.firebaseapp.com",
-  projectId: "jtl-management",
-  storageBucket: "jtl-management.firebasestorage.app",
-  messagingSenderId: "343698606044",
-  appId: "1:343698606044:web:168bc36d64e5e0723a3119",
-  measurementId: "G-89CPWZJ5WR"
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  define: {
+    "process.env": process.env,
+  },
+});
